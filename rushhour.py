d5c3112eb83195b2bfcc13c89f4149cc361e1d5e
stanhelsloot <43984796+stanhelsloot@users.noreply.github.com>
2018-10-31 15:35:32 +0100

    creation of russhour.py

---
+++ rushhour.py
@@ -0,0 +1,25 @@
+class Rushhour(object):
+    """docstring for Rushhour."""
+    def __init__(self, arg):
+        self.arg = arg
+
+    def load_board():
+        """Function for loading the board"""
+        pass
+
+    def load_cars():
+        """Function for loading the cars on the board"""
+        pass
+
+    def move():
+        """Function for moving the cars on the board"""
+        pass
+
+    def won():
+        """Win condition for the game"""
+        pass
+
+
+    def play():
+        """Lets play a game"""
+        pass
