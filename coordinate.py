17a86a35ed934d8d8c40b7235d797a22f9c096b8
Tula Kaptein <tula.kaptein@gmail.com>
2018-10-31 14:45:41 +0100

    creation

---
+++ coordinate.py
@@ -0,0 +1,10 @@
+class Coordinate(object):
+    """
+    Describes a coordinateself.
+    """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.occupied = False
+    def occupy(bool):
+        self.occupied = bool
---
+++ location.py
@@ -0,0 +1,8 @@
+class Location(object):
+    """
+    Describes a location
+    """
+    def __init__(self, coordinate1, coordinate2, [coordinate3]):
+        self.coordinate1 = coordinate1
+        self.coordinate2 = coordinate2
+        self.coordinate3 = coordinate3
