9fa55e6c455a77b758e26444ac44138898121155
Tula Kaptein <tula.kaptein@gmail.com>
2018-11-07 15:08:26 +0100

    willekeurig functie

--- code/algoritmes/willekeurig.py
+++ code/algoritmes/willekeurig.py
@@ -12,25 +12,13 @@ def willekeurig():
         amount_cars = len(self.car_list)
         rand_id = randint(1, amount_cars)
 
-        # find length of this car (2 or 3)
-        for car in self.car_list:
-            if rand_id == car.id:
-                length = car.length
+        # give a random command
+        random = randint(1,2)
+        if random == 1:
+            command = '+'
+        elif:
+            command = '-'
 
-        # pick a random coordinate this car should go to
-        # inside the borders of the board: 6x6
-        command = []
-        # give three coordinates if car is length 3
-        if length > 2:
-            coordinate = randint(0, 3)
-            command.append(coordinate)
-            command.append(coordinate + 1)
-            command.append(coordinate + 2)
-        # give two coordinates if car is length 2
-        else:
-            coordinate = randint(0, 4)
-            command.append(coordinate)
-            command.append(coordinate + 1)
         self.move(command, rand_id)
         self.update_board()
         self.print_board()
