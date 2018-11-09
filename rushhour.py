82c829871a4f4440f1778feba2f2b655bf48020f
stanhelsloot <43984796+stanhelsloot@users.noreply.github.com>
2018-11-09 16:15:32 +0100

    check move is full of insects

--- rushhour.py
+++ rushhour.py
@@ -78,8 +78,9 @@ class Rushhour(object):
         if self.check_move(move, id) and self.inside_boundries(id, move):
             # do the move
             car.update_coordinates(move)
+            return True
         else:
-            print("invalid move!!")
+            return False
 
     def check_move(self, move, car_id):
         """checks if the move is legal"""
@@ -92,45 +93,42 @@ class Rushhour(object):
                 if len(car.coordinate) == 2:
                     if car.coordinate[0][0] == car.coordinate[1][0]:
                         for i in range(abs(car.coordinate[0][1] - move[0])):
+                            print(car.coordinate[0][1] - move[0])
                             doof = [0, 0]
-                            doof[0] = i
-                            doof[1] = i + 1
+                            doof[0] = car.coordinate[0][1] + i
+                            doof[1] = car.coordinate[1][1] + i
+                            print(doof)
                             for coordinate in car_not.coordinate:
                                 if coordinate in car.temp_coordinates(doof):
                                     return False
                     else:
-                        for i in range(car.coordinate[0][0] - move[0]):
+                        for i in range(abs(car.coordinate[0][0] - move[0])):
                             doof = [0, 0]
-                            doof[0] = i
-                            doof[1] = i + 1
+                            doof[0] = car.coordinate[0][1] + i
+                            doof[1] = car.coordinate[1][1] + i
                             for coordinate in car_not.coordinate:
                                 if coordinate in car.temp_coordinates(doof):
                                     return False
-                                    for coordinate in car_not.coordinate:
-                                        if coordinate in car.temp_coordinates(doof):
-                                            return False
                 else:
                     if car.coordinate[0][0] == car.coordinate[1][0]:
-                        for i in range(car.coordinate[0][1] - move[0]):
+                        for i in range(abs(car.coordinate[0][1] - move[0])):
                             doof = [0, 0, 0]
-                            doof[0] = i
-                            doof[1] = i + 1
-                            doof[3] = i + 2
+                            doof[0] = car.coordinate[0][0] + i
+                            doof[1] = car.coordinate[1][0] + i
+                            doof[2] = car.coordinate[2][0] + i
                             for coordinate in car_not.coordinate:
                                 if coordinate in car.temp_coordinates(doof):
                                     return False
                     else:
-                        for i in range(car.coordinate[0][0] - move[0]):
-                            doof = [0, 0]
-                            doof[0] = i
-                            doof[1] = i + 1
-                            doof[2] = i + 2
+                        for i in range(abs(car.coordinate[0][0] - move[0])):
+                            doof = [0, 0, 0]
+                            doof[0] = car.coordinate[0][1] + i
+                            doof[1] = car.coordinate[1][1] + i
+                            doof[2] = car.coordinate[2][1] + i
+                            print(doof)
                             for coordinate in car_not.coordinate:
                                 if coordinate in car.temp_coordinates(doof):
                                     return False
-                                    for coordinate in car_not.coordinate:
-                                        if coordinate in car.temp_coordinates(doof):
-                                            return False
         return True
 
     def check_path(self, command, car_id):
@@ -219,26 +217,38 @@ class Rushhour(object):
         """
         Genereert willekeurige commands voor de auto's in het spel
         """
-        # while not self.won
         counter = 0
+        # while not self.won
         while not self.won():
             # pick a random car (in case of 3 cars)
-
-            amount_cars = len(self.car_list)
+            amount_cars = len(self.car_list) - 1
             rand_id = randint(1, amount_cars)
-
-            # give a random command
-            random = randint(1,2)
-            if random == 1:
-                command = '+'
+            # find length of this car (2 or 3)
+            for car in self.car_list:
+                if int(rand_id) == int(car.id):
+                    length = int(car.length)
+            # pick a random coordinate this car should go to
+            # inside the borders of the board: 6x6
+            command = []
+            # give three coordinates if car is length 3
+            if length > 2:
+                coordinate = randint(0, 3)
+                command.append(coordinate)
+                command.append(coordinate + 1)
+                command.append(coordinate + 2)
+            # give two coordinates if car is length 2
             else:
-                command = '-'
-
+                coordinate = randint(0, 4)
+                command.append(coordinate)
+                command.append(coordinate + 1)
             self.move(command, rand_id)
-            counter += 1
-            self.update_board()
-            print(counter)
-            self.print_board()
+            if self.move(command, rand_id):
+                pass
+            else:
+                counter += 1
+                self.update_board()
+                print(counter)
+                self.print_board()
 
     def check_command(self, command):
         """Checks if the command is valid"""
