fd7c1ea358fa86b467f7f13e81a110b34c4b6b27
Tula Kaptein <tula.kaptein@gmail.com>
2018-11-12 21:40:41 +0100

    lol

--- code/algoritmes/willekeurig_large_moves.py
+++ code/algoritmes/willekeurig_large_moves.py
@@ -1,15 +1,15 @@
-def willekeurig(self):
+def Willekeurig(game):
     """
     Genereert willekeurige commands voor de auto's in het spel
     """
     counter = 0
     # while not self.won
-    while not self.won():
+    while not game.won():
         # pick a random car (in case of 3 cars)
-        amount_cars = len(self.car_list)
+        amount_cars = len(game.car_list)
         rand_id = randint(1, amount_cars)
         # find length of this car (2 or 3)
-        for car in self.car_list:
+        for car in game.car_list:
             if rand_id == car.id:
                 length = car.length
         # pick a random coordinate this car should go to
@@ -26,8 +26,8 @@ def willekeurig(self):
             coordinate = randint(0, 4)
             command.append(coordinate)
             command.append(coordinate + 1)
-        self.move(command, rand_id)
-        self.update_board()
-        self.print_board()
+        game.move(command, rand_id)
+        game.update_board()
+        game.print_board()
         print(counter)
         counter += 1
