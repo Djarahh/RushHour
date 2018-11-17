32f0a3bd7aedfab95890d782e79ad4e6a6023a93
Tula Kaptein <tula.kaptein@gmail.com>
2018-11-17 15:56:09 +0100

    main.py nog aangepast naar wat ik in mijn test.py had staan

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from code.rushhour import Rushhour
-# from code.algorithms.random_things import Randomize
 from code.algorithms.random_possible_moves import Randomize
 from code.classes.load_cars import LoadCars
-from code.classes.visualize_sequence import SequenceVisualization
+# from code.classes.visualize_sequence import SequenceVisualization
+from code.classes.solution_txt import TxtSolution
 
 
 def main(game_id):
+
+    # Load the cars and the board
     things = LoadCars(game_id)
     car_list = things.car_list
     board = things.board
-    # how to import willekeurig
-    rushhour = Rushhour(car_list, board)
-    Randomize(rushhour)
 
-    # Visualize the solution that the algorithm made
-    SequenceVisualization(game_id)
+    # Initiate a RushHour game
+    rushhour = Rushhour(car_list, board)
+
+    # Let the randomize algorithm run and return a solution
+    A = Randomize(rushhour)
+    solution = A.randomize()
+
+    print(len(solution))
+
+    # Make a .txt file with the solution
+    TxtSolution(game_id, solution)
+
+    # # Visualize the solution that the algorithm made
+    # SequenceVisualization(game_id)
 
 
 if __name__ == "__main__":
