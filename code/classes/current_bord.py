c6c54697b9452dd9470eb7b82227fb7db6c63944
Tula Kaptein <tula.kaptein@gmail.com>
2018-11-20 10:52:10 +0100

    current_bord verwijderd, en classes solution_text aangepast

--- code/classes/current_bord.py
+++
@@ -1,8 +0,0 @@
-class Current_bord(object):
-    """Describes the state of the Current_bord."""
-    def __init__(self, length, entrance):
-        self.length = length
-        self.entrence = entrance
-        self.coordinates = Coordinates()
-
-        
