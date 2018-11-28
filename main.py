c8b0fce36c5ec785d3fb2bcc082408bed84a98c8
Djarahh <43991274+Djarahh@users.noreply.github.com>
2018-11-28 15:36:06 +0100

    de gehardcode dingen in won and make_possible_moves in rushhour.py zijn eruit gehaald

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def main(game_id):
     # Initiate a RushHour game
     rushhour = Rushhour(deepcopy(car_list), deepcopy(board))
 
-<<<<<<< HEAD
     # Let the randomize algorithm run and return a solution
     # A = Randomize(deepcopy(rushhour))
     # solution = A.randomize()
@@ -35,78 +34,22 @@ def main(game_id):
     B = Graph(deepcopy(rushhour))
     solution = B.bfs()
     algorithm = "BFS"
-=======
-    # # Let the randomize algorithm run and return a solution
-<<<<<<< HEAD
-    # A = Randomize(deepcopy(rushhour))
-    # solution = A.randomize()
-    # algorithm = "RANDOM"
-=======
-    A = Randomize((rushhour))
-    solution = A.randomize()
-    #algorithm = "RANDOM"
-    final_board = rushhour.return_car_list()
-    # use this algorithm for the final board of rushhour
-    rush = Rushhour(deepcopy(car_list), deepcopy(board))
-<<<<<<< HEAD
-    B = ValueGiver(deepcopy(car_list), deepcopy(final_board), rush)
-    w = B.compare_i_f()
-    print(B.board_value)
-=======
-    B = Iterative(deepcopy(car_list), deepcopy(final_board), rush)
->>>>>>> 76f930308c4dcade1c2ccc80afaa929d78ce3995
->>>>>>> 8ba99cd4484d0ac0fb6e39c76693c27df27be23c
-
-    # Let the BFS algorithm work
-    # B = Graph(deepcopy(rushhour))
-    # solution = B.bfs()
-    # algorithm = "BFS"
->>>>>>> 14a8d418e52b3ac4cb9fbcf284da88279e1398b7
 
     # # Let the DFS algorithm work it
     # C = Tree(deepcopy(rushhour))
     # solution = C.dfs()
     # algorithm = "DFS"
 
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
     # # Branch and Bound algorithm
     # D = Branches(deepcopy(rushhour))
     # solution = D.bnb()
     # algorithm = "BNB"
 
-    # # Make a .txt file with the solution
-    # TxtSolution(game_id, solution, algorithm)
-    #
-    # # Visualize the solution that the algorithm made
-    # SequenceVisualization(game_id, rushhour, algorithm)
-
-if __name__ == "__main__":
-    main("2")
-=======
->>>>>>> 14a8d418e52b3ac4cb9fbcf284da88279e1398b7
-    # # Make a .txt file with the solution
-    # TxtSolution(game_id, solution)
-
-    # # Visualize the solution that the algorithm made
-    # SequenceVisualization(game_id, rushhour)
-<<<<<<< HEAD
-
     # Make a .txt file with the solution
     TxtSolution(game_id, solution, algorithm)
-=======
->>>>>>> 14a8d418e52b3ac4cb9fbcf284da88279e1398b7
 
     # Visualize the solution that the algorithm made
-    # SequenceVisualization(game_id, rushhour, algorithm)
-
+    SequenceVisualization(game_id, rushhour, algorithm)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    main("1")
-=======
     main("7")
->>>>>>> 76f930308c4dcade1c2ccc80afaa929d78ce3995
->>>>>>> 14a8d418e52b3ac4cb9fbcf284da88279e1398b7
