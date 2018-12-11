093ceffd56721ea910b7608a3dc5e1d4b358812f
Djarahh <43991274+Djarahh@users.noreply.github.com>
2018-12-11 19:09:59 +0100

    BeFS fix in de main

--- main.py
+++ main.py
@@ -18,26 +18,30 @@ def main(game_id):
     rushhour = cd.cs.Rushhour(deepcopy(car_list), deepcopy(board))
 
     # Let the randomize algorithm run and return a solution
-    # A = cd.alg.Randomize(deepcopy(rushhour), deepcopy(car_list))
-    # solution= A.randomize()
-    # algorithm = "RANDOM"
-    # #
-    # final_board = rushhour.return_car_list()
-    # cd.cs.TxtSolution(game_id, solution, algorithm)
+    A = cd.alg.Randomize(deepcopy(rushhour), deepcopy(car_list))
+    solution= A.randomize()
+    algorithm = "RANDOM"
+    #
+    final_board = A.game.return_car_list()
+
+    for car in final_board:
+        print(car.id)
+        print(car.coordinate)
+    cd.cs.TxtSolution(game_id, solution, algorithm)
 
     # s = LoadSolution(game_id)
     # solved_board = s.car_list
     #
-    # rush = cd.cs.Rushhour(deepcopy(car_list), deepcopy(board))
-    # B = cd.alg.BestFirst(rush, final_board)
-    # solution = B.bfs()
-    # algorithm = "BEST"
+    rush = cd.cs.Rushhour(deepcopy(car_list), deepcopy(board))
+    B = cd.alg.BestFirst(rush, final_board)
+    solution = B.bfs()
+    algorithm = "BEST"
 
 
     # Let the BFS algorithm work
-    B = cd.alg.Graph(deepcopy(rushhour))
-    solution = B.bfs()
-    algorithm = "BFS"
+    # B = cd.alg.Graph(deepcopy(rushhour))
+    # solution = B.bfs()
+    # algorithm = "BFS"
 
 
     # # Let the DFS algorithm work it
