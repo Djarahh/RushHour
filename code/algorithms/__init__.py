a58a04ab78a5cd7e575a93d6eccc4a2c40a49db5
Tula Kaptein <tula.kaptein@gmail.com>
2018-12-17 17:49:07 +0100

    joeipie

--- code/algorithms/__init__.py
+++ code/algorithms/__init__.py
@@ -2,5 +2,5 @@ from .BFS import Graph
 from .DFS import Tree
 from .branch_and_bound import Branches
 from .randomize import Randomize
-from .beam_search import BeamSearch
-from .best_first import BestFirst
+from .beam import BeamSearch
+from .best import BestFirst
